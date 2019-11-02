3cf920818e6281933739d91d69f0d1268a6b2465
Brooks Geenen <brgeen@gmail.com>
2019-11-01 22:28:19 -0500

    set local state and send payload to global store via action and reducer

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
 
 class Feeling extends Component {
+
+    state = {
+        feeling: '',
+    }
+
+
+    handleInput = (event) => {
+        this.setState({
+          feeling: event.target.value, 
+        })
+    }
+
+    handleClick = () => {
+        this.props.dispatch({type: "FEELING", payload: this.state.feeling })
+    }
     render() {
         return (
 
             <div>
                 <h3>How are you feeling today?</h3>
-                <input type="number"></input>
-                <Link to="/Understanding"><button>Next</button></Link>
-
+                <input 
+                onChange={(event) => this.handleInput(event)}
+                value={this.state.feeling}
+                type="number"></input>
+                <Link to="/Understanding"><button onClick={this.handleClick}>Next</button></Link>
+            <pre>{JSON.stringify(this.state.feeling, null, 2)}</pre>
             </div>
 
         );
     }
 }
 
-export default Feeling;
+export default connect()(Feeling);
--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+
+const reducer = (state =[], action)=>{
+    if(action.type==="FEELING"){
+        return action.payload
+    }
+    return state
+}
 
 
 
 const storeInstance = createStore(
     combineReducers({
-      
+      reducer,
     }),
-    // applyMiddleware(logger),
+    applyMiddleware(logger),
 );
 
 
